dab28e35f1ec117ecdbe44a3419e7216a563a0d1
mspano <mspanopablo@gmail.com>
2023-09-20 22:47:36 -0300

    DB started

--- Server/src/index.js
+++ Server/src/index.js
@@ -1,5 +1,8 @@
 const server = require("./app");
 const PORT = 3001;
+const { conn } = require("./DB_connection");
+
+conn.sync({ force: true }); //el force true dropea la base de datos, cuando la db esta ok, la pongo en false
 
 server.listen(PORT, () => {
   console.log("Server raised in port: " + PORT);
